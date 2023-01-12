9b19c8a52cdaef42227296be1fa9933a4d58ed44
gabi-lebeshkovska <122521744+gabi-lebeshkovska@users.noreply.github.com>
2023-01-12 11:01:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasketballEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int YearTax = int.Parse(Console.ReadLine());
+            double Shoes = YearTax - (YearTax * 0.4);
+            double Clothes = Shoes - (Shoes * 0.2);
+            double Ball = Clothes * 0.25;
+            double Aksess = Ball * 0.2;
+
+            double WholeChash = YearTax + Shoes+ Clothes + Ball+ Aksess;
+
+            Console.WriteLine(WholeChash);
+            
+            
+
+
+        }
+    }
+}
